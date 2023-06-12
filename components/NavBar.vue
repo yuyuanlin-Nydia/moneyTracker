1321110a2e8579ff4379e058a5d1deac0e45db95
YuYuan Lin <sandy6513a@yahoo.com.tw>
2023-06-12 21:42:17 +0800

    feat: adjust nav style and become component

---
+++ components/NavBar.vue
@@ -0,0 +1,49 @@
+<template>
+  <nav class="sticky top-0 h-screen flex flex-col text-lg col-span-1  text-right text-primary-100 border-r-2 border-primary-400 px-4">
+    <NuxtLink to="/" class=" font-bold text-white py-6 pr-2">
+      <Icon name="solar:chat-round-money-bold" class="text-4xl rounded bg-primary-200 md:text-xl" />
+      <span class="hidden md:inline">
+        MoneyTracker
+      </span>
+    </NuxtLink>
+    <NuxtLink v-slot="{ isActive }" to="/" title="Overview">
+      <div :class="['mt-5', isActive? 'text-secondary-100':'hover:text-primary-200']">
+        <span class="pr-1 hidden md:inline">Overview</span>
+        <Icon class="mr-2 text-4xl md:text-xl" name="material-symbols:folder-copy" />
+      </div>
+    </NuxtLink>
+    <NuxtLink v-slot="{ isActive }" to="/wallet" title="Wallet">
+      <div :class="['mt-5', isActive? 'text-secondary-100':'hover:text-primary-200']">
+        <span class="pr-1 hidden md:inline">Wallet</span>
+        <Icon class="mr-2 text-4xl md:text-xl" name="material-symbols:account-balance-wallet" />
+      </div>
+    </NuxtLink>
+    <NuxtLink v-slot="{ isActive }" to="/budget" title="Budget">
+      <div :class="['mt-5', isActive? 'text-secondary-100':'hover:text-primary-200']">
+        <span class="pr-1 hidden md:inline">Budget</span>
+        <Icon class="mr-2 text-4xl md:text-xl" name="material-symbols:list-alt-rounded" />
+      </div>
+    </NuxtLink>
+    <NuxtLink v-slot="{ isActive }" to="/account" title="Account">
+      <div :class="['mt-5', isActive? 'text-secondary-100':'hover:text-primary-200']">
+        <span class="pr-1 hidden md:inline">Account</span>
+        <Icon class="mr-2 text-4xl md:text-xl" name="material-symbols:account-box" />
+      </div>
+    </NuxtLink>
+    <!-- <NuxtLink v-if="!user.isLoggedIn" class="pt-1" to="/login" title="Login">
+      <div :class="['mt-5', isActive? 'text-secondary-100':'hover:text-primary-200']">
+         <span class="pr-1">Login</span>
+          <Icon name="clarity:login-solid" />
+      </div>
+    </NuxtLink> -->
+    <a
+      href="#"
+      title="Logout"
+      class="mr-2 absolute bottom-8 right-0 border-r-4 border-transparent hover:text-primary-200 "
+      @click.prevent="user.isLoggedIn = false"
+    >
+      <span class="pr-1 hidden md:inline">Logout</span>
+      <Icon class="mr-2 text-4xl md:text-xl" name="clarity:logout-solid" />
+    </a>
+  </nav>
+</template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -14,41 +14,7 @@ watch(
 
 <template>
   <div class="grid min-h-screen grid-cols-8 h-full bg-primary-600 text-white">
-    <nav class="sticky top-0 h-screen flex flex-col text-lg col-span-1  text-right text-primary-100 border-r-2 border-primary-400 px-4">
-      <NuxtLink to="/" class=" font-bold text-white py-6 pr-2">
-        <Icon name="solar:chat-round-money-bold" class="text-4xl rounded bg-primary-200 md:text-xl" />
-        <span class="hidden md:inline">
-          MoneyTracker
-        </span>
-      </NuxtLink>
-      <NuxtLink to="/" class="mt-5 border-r-4 border-transparent hover:text-secondary-100 hover:border-secondary-100">
-        <span class="pr-1 hidden md:inline">Overview</span>
-        <Icon class="mr-2 text-4xl md:text-xl" name="material-symbols:folder-copy" />
-      </NuxtLink>
-      <NuxtLink to="/wallet" class="border-r-4 border-transparent hover:text-secondary-100 hover:border-secondary-100">
-        <span class="pr-1 hidden md:inline">Wallet</span>
-        <Icon class="mr-2 text-4xl md:text-xl" name="material-symbols:account-balance-wallet" />
-      </NuxtLink>
-      <NuxtLink to="/budget" class="mt-1 border-r-4 border-transparent hover:text-secondary-100">
-        <span class="pr-1 hidden md:inline">Budget</span>
-        <Icon class="mr-2 text-4xl md:text-xl" name="material-symbols:list-alt-rounded" />
-      </NuxtLink>
-      <NuxtLink to="/account" class="mt-1 border-r-4 border-transparent hover:text-secondary-100">
-        <span class="pr-1 hidden md:inline">Account</span>
-        <Icon class="mr-2 text-4xl md:text-xl" name="material-symbols:account-box" />
-      </NuxtLink>
-      <!-- <NuxtLink v-if="!user.isLoggedIn" class="pt-1" to="/login">
-        <span class="pr-1">Login</span>
-        <Icon name="clarity:login-solid" />
-      </NuxtLink> -->
-      <a
-        href="#" class="mr-2 absolute bottom-8 right-0 border-r-4 border-transparent hover:text-secondary-100 hover:border-secondary-100"
-        @click.prevent="user.isLoggedIn = false"
-      >
-        <span class="pr-1 hidden md:inline">Logout</span>
-        <Icon class="mr-2 text-4xl md:text-xl" name="clarity:logout-solid" />
-      </a>
-    </nav>
+    <NavBar />
     <main class="px-8 col-span-7">
       <h5 class=" text-4xl font-bold py-6">
         {{ routeName }}
